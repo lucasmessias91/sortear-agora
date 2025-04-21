fb703e29489f4417f44b6f0ad34369b21fdf82b9
lucasmessias91 <lmessias91@gmail.com>
2025-04-21 14:04:24 -0300

    Adicionado condição de >

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function sortearAgora() {
+    const numMin = Math.ceil(document.querySelector(".num-min").value);
+    const numMax = Math.floor(document.querySelector(".num-max").value);
+  if (numMax > numMin) {
     const resultado = document.querySelector(".num-sorteado");
-    const numMin = Math.ceil(document.querySelector(".num-min").value)
-    const numMax = Math.floor(document.querySelector(".num-max").value)
-    const numSorteado = Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
+    
+    const numSorteado =
+      Math.floor(Math.random() * (numMax - numMin + 1)) + numMin;
     resultado.innerHTML = numSorteado;
+  } else {
+    alert("O número máximo deve ser MAIOR que o número mínimo.");
+  }
 }
